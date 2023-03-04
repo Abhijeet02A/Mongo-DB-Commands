d7c30be9a6f6b0ccf165e5cba839dacae83a266b
abhijeet <abhijitawari1402@gmail.com>
2023-03-04 17:16:08 +0530

    create mongo db account on atlas

---
+++ MongoDB CLI .cmd
@@ -0,0 +1,6 @@
+Create an Atlas account =>  {atlas auth register} 
+                            :: and then follow the process
+                            :: copy code and click on register link to register...
+
+
+
