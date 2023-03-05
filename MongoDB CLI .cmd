5896ecc1d4a6c335e1b931d90dd96aa3b239e5e0
abhijeet <abhijitawari1402@gmail.com>
2023-03-05 23:36:36 +0530

    add cluster

--- MongoDB CLI .cmd
+++ MongoDB CLI .cmd
@@ -1,6 +1,25 @@
+::Beacket is not part of command
+
 Create an Atlas account =>  {atlas auth register} 
                             :: and then follow the process
                             :: copy code and click on register link to register...
+
+
+::use the following command to deploy a MongoDB Atlas cluster. Then follow the instructions in the terminal.
+{atlas setup --clusterName myAtlasClusterEDU --provider AWS --currentIp --skipSampleData --username myAtlasDBUser --password myatlas-001 | tee atlas_cluster_details.txt}
+
+::it will ask yes no question say Y 
+::username :- myAtlasDBUser
+::password :- myatlas-001
+
+
+::
+::use the following command to load the sample data into the Atlas cluster that you just deployed:
+{atlas clusters loadSampleData myAtlasClusterEDU}
+::You will receive Successfull creation message
+
+::Authenticate in atlas CLI 
+{atlas auth login}
 
 
 
